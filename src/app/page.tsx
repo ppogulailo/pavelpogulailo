c8a4ac44083ba37eaf4c5648d82f0f543e261829
ppogulailo <pavel.pogulailo@gmail.com>
2024-02-28 16:10:03 +0200

    T-1 eslint/prettier config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,56 @@
-import Image from "next/image";
+import Image from 'next/image'
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+export default async function Index() {
+    const images = ['github.svg', 'linkedin.svg', 'twitter.svg']
+    return (
+        <>
+            <div className="mb-16 w-full mt-72">
+                <div
+                    className=" max-w-[1237px] m-auto bg-gray-100  rounded-[8px] border border-gray-300 bg-gradient-to-b from-[#D9D9D905] to-[#ba3a3a05] bg-[300%_auto] p-4 shadow-lg dark:border-[#ffffff22] md:rounded-[40px] lg:bg-cover lg:p-12"
+                    style={{
+                        backgroundImage: 'url(/img/grid-dash.svg)',
+                        backgroundPosition: 'top',
+                        backgroundRepeat: 'revert-layer',
+                        zIndex: '0',
+                    }}
+                >
+                    <div className="m-auto flex justify-center">
+                        <Image
+                            className="rounded-full"
+                            src="/assets/profile.jpg" // Correct path relative to the `public` directory
+                            alt="Profile Image"
+                            width={200} // Adjust the width as needed
+                            height={200} // Adjust the height as needed
+                        />
+                    </div>
+                    <div className="text-3xl text-center mt-2">Pavel Pogulailo</div>
+                    <div className="flex">
+                        {images.map((path, key) => (
+                            <Image
+                                key={key}
+                                className="rounded-full"
+                                src={`/assets/logos/${path}`}
+                                alt="Github-Image"
+                                width={30}
+                                height={30}
+                            />
+                        ))}
+                    </div>
+                    {/*<div className='flex w-full items-center justify-center p-4 lg:pb-8 '>*/}
+                    {/*    <div className='w-full max-w-[1237px]'>*/}
+                    {/*        <div className="relative grid grid-cols-1 gap-8 rounded-[25px] border-[2px] border-transparent bg-gray-100 bg-clip-padding p-8 text-sm text-gray-900 before:absolute before:inset-0 before:z-[-1] before:m-[-2px] before:rounded-[inherit] before:bg-gradient-to-r before:from-violet-500 before:to-fuchsia-500 before:content-[''] dark:bg-[#09101F] dark:text-[#AAB7D2] md:grid-cols-3 lg:p-12">*/}
+                    {/*            <Image*/}
+                    {/*                src="/assets/profile.jpg" // Correct path relative to the `public` directory*/}
+                    {/*                alt="Profile Image"*/}
+                    {/*                width={200} // Adjust the width as needed*/}
+                    {/*                height={200} // Adjust the height as needed*/}
+                    {/*            />*/}
+                    {/*            <div className='text-3xl'>Pavel Pogulailo</div>*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
+                </div>
+            </div>
+        </>
+    )
 }
---
+++ src/data/constants.ts
@@ -0,0 +1,81 @@
+declare type Locale = string;
+
+interface Meta {
+    URL: string | URL;
+    siteName: string;
+    title?: string;
+    description?: string;
+    themeColor?: string;
+    backgroundColor?: string;
+    og: {
+        locale?: Locale;
+        type?: 'website';
+        ogImage: string | URL;
+        width?: number;
+        height?: number;
+    };
+    twitter: {
+        card?: string;
+        site?: string;
+    };
+}
+
+// Nullish coalescing: the `??` operator is a way to “fall back” to
+// a default value when dealing with `null` or `undefined`.
+
+// Best approach: create a config file in your project where you export an
+// object containing all configuration values for your project. There, you
+// can set default values for variables coming from the environment. In this way,
+// you guarantee a value will be present for the execution of your program:
+
+// export const config = {
+//   nodeEnv: process.env.NODE_ENV ?? 'development',
+//   openAIKey: process.env.OPENAI_API_KEY ?? '',
+//   statusPage: process.env.STATUS_PAGE_ID ?? '',
+// };
+
+// Our basic SEO data
+export const meta: Meta = {
+    URL: 'https://www.dreamaweso.me',
+    siteName: 'DreamAwesome',
+    title: 'Unlock the potential of your dreams using AI',
+    description:
+        'DreamAwesome helps you analyze your dreams to gain insight into the their meaning to achieve personal growth',
+    themeColor: '##F5E1E6',
+    backgroundColor: '#F5E1E6',
+    og: {
+        locale: 'en-US',
+        type: 'website',
+        ogImage: '/ogimage.jpg',
+        width: 1200,
+        height: 630,
+    },
+    twitter: {
+        card: 'summary_large_image',
+        site: '@dreamawesome',
+    },
+};
+
+export const menuItems = [
+    { path: '/', name: 'Home', new: false },
+    { path: '/about', name: 'About', new: false },
+    { path: '/blog', name: 'Blog', new: true },
+];
+
+export const footerItems = {
+    resources: [
+        { path: '/', name: 'Documentation', internal: true },
+        { path: '/', name: 'Support', internal: true },
+        { path: '/', name: 'Contact Us', internal: true },
+    ],
+    company: [
+        { path: '/', name: 'Home', internal: true },
+        { path: '/blog', name: 'Blog', internal: true },
+        { path: '/', name: 'Careers', internal: true },
+    ],
+    legal: [
+        { path: '/privacy', name: 'Privacy Policy', internal: true },
+        { path: '/terms', name: 'Terms of Service', internal: true },
+        { path: '/cookies', name: 'Cookies', internal: true },
+    ],
+};
